9ac10dd3d483a5a7b35fd11e07d4e2fe0db4f678
Arsham Shirvani <arshamshirvani@gmail.com>
2022-06-18 23:11:21 +0100

    feat(scripts): add update_arch script

---
+++ update_arch.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/bash
+
+rm -rf tmp/*
+asp update
+
+declare -A paths
+paths=(
+  ["cairo"]=extra/cairo
+)
+
+for path in ${!paths[@]}; do
+  package=${paths[${path}]}
+  echo Packaging $path from $package
+
+  pushd arch/$path
+  if [[ -f "prepare.sh" ]]; then
+      ./prepare.sh
+      if [ $? -ne 0 ]; then
+        echo Error preparing $path
+        exit 1
+      fi
+  fi
+
+  makepkg -si --noconfirm
+  if [ $? -ne 0 ]; then
+    echo Error building $path
+    exit 1
+  fi
+
+  pushd ../../tmp
+  asp export $package
+  if [ $? -ne 0 ]; then
+    exit 1
+  fi
+
+  cp $path/* ../arch/$path/
+  if [ $? -ne 0 ]; then
+    exit 1
+  fi
+  popd
+
+  git diff --quiet .
+  if [ $? -ne 0 ]; then
+    notify-send -t 5000 $path "remote was changed"
+  fi
+
+  rm -rf ../../tmp/$path
+  popd
+done
