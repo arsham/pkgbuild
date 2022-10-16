caf7e411131b788bf2ca3bd8b891dfc7b455c570
Arsham Shirvani <arshamshirvani@gmail.com>
2022-10-16 17:20:13 +0100

    ref(vifm): add prepare script

---
+++ aur/vifm-git/prepare.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Packages required by vifm.
+paru -S --noconfirm --needed \
+    zathura zathura-cb zathura-djvu zathura-pdf-poppler zathura-ps \
+    python-pdftotext \
+    gv \
+    apvlv \
+    mplayer \
+    mp3info \
+    mpv-git \
+    ristretto \
+    ark \
+    docx2txt \
+    pcmanfm-git \
+    shellpic \
+    dragon-drop
